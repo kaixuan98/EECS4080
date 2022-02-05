fe3155ddd49234e8d1a0e8ff1c9db5598703f4e3
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2022-02-05 17:14:11 -0500

    Able to stream tweets

---
+++ data_collecting/twitter_request.py
@@ -0,0 +1,67 @@
+from decouple import config
+import tweepy
+import pandas as pd
+import time
+
+api_key = config('API_KEY')
+api_key_secret = config('API_KEY_SECRET')
+access_token = config('ACCESS_TOKEN')
+access_token_secret = config('ACCESS_TOKEN_SECRET')
+bearer_token = config('BEARER_TOKEN')
+
+
+# search 
+# client = tweepy.Client(bearer_token=bearer_token)
+
+# query = 'climatechange OR ecofriendly OR sustainable OR zerowaste OR environment OR climateaction OR savetheplanet -is:retweet'
+
+# response = client.search_recent_tweets(query=query, max_results=20, tweet_fields=['lang', 'context_annotations', 'geo'], expansions=['author_id' ])
+
+# columns = ['tweetId', 'tweet', 'lang', '']
+# data = []
+# for res in response.data: 
+#     print(res.id)
+#     print(res.text + "\n")
+
+
+
+class Linstener(tweepy.Stream):
+
+    tweets = []
+    limit = 150
+
+    def on_status(self, status):
+        self.tweets.append(status)
+        # print(status.user.screen_name + ": " + status.text)
+
+        if len(self.tweets) == self.limit:
+            self.disconnect()
+
+# TODO: create a schedular that run the request every 15 mins 
+# TODO: Start cleaning data
+
+
+if __name__ == "__main__": 
+    # authentication
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # create a listener to stream tweets 
+    stream_tweet = Linstener(api_key, api_key_secret, access_token, access_token_secret)
+    keywords = ['climatechange' , 'ecofriendly' , 'sustainable','climateaction', 'savetheplanet' , '-is:retweet']
+    columns = ['tweet_id' ,'user_id','user' , 'tweet' , 'lang' ]
+    data = []
+
+    # filter stream with keywords
+    stream_tweet.filter(track=keywords)
+    for tweet in stream_tweet.tweets:
+        print(tweet)
+        if not tweet.truncated:
+            data.append([tweet.id, tweet.user.id ,tweet.user.screen_name, tweet.text, tweet.lang])
+        else:
+            data.append([tweet.id, tweet.user.id ,tweet.user.screen_name, tweet.extended_tweet['full_text'], tweet.lang])
+    df = pd.DataFrame(data, columns=columns)
+    print(df)
+
+    df.to_csv('raw_data.csv', index=False)
